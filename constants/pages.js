585050c297bbe7b386a8fd30311d5d62fca36e39
luongthtai <luongthetai2705@gmail.com>
2023-01-04 17:16:03 +0700

    fix name page

--- constants/pages.js
+++ constants/pages.js
@@ -1,9 +1,9 @@
 import About from "../components/About/About";
-import Blog from "../components/Blog/blog";
-import Contact from "../components/Contact/contact";
+import Blog from "../components/Blog/Blog";
+import Contact from "../components/Contact/Contact";
 import HeroHeader from "../components/HeroHeader/HeroHeader";
-import Portfolid from "../components/PortFolid/portfolid";
-import Services from "../components/Services/services";
+import Portfolid from "../components/PortFolid/Portfolid";
+import Services from "../components/Services/Services";
 
 const pages = {
     HeroHeader: <HeroHeader />,
